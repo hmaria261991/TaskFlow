73cba07939551f6d68b9876348d8c1576379ee8e
hmaria261991 <mhernandezz@ucenfotec.ac.cr>
2026-02-03 13:54:30 -0600

    created userService

---
+++ src/main/java/org/spring/taskflow/Service/UserService.java
@@ -0,0 +1,39 @@
+package org.spring.taskflow.Service;
+
+
+import org.spring.taskflow.Models.User;
+import org.spring.taskflow.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public List<User>  getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User>  getlUser(Long id){
+        return userRepository.findById(id);
+    }
+
+    public void updateUser(User  user){
+        userRepository.save(user)
+    }
+
+    public void createUser(User  user){
+        userRepository.save(user);
+    }
+
+    public void deleteUserByID(Long id){
+        userRepository.deleteById(id);
+    }
+
+
+}
