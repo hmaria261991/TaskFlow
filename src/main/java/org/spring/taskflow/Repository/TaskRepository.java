93c6377efad6e8b4689c30f4d796ed927a9823c9
hmaria261991 <mhernandezz@ucenfotec.ac.cr>
2026-02-03 12:51:06 -0600

    created repositories

---
+++ src/main/java/org/spring/taskflow/Repository/TaskRepository.java
@@ -0,0 +1,10 @@
+package org.spring.taskflow.Repository;
+
+import org.spring.taskflow.Models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+}
